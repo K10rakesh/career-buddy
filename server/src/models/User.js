bded7b51fdeb7619731e3a21b08c4092dd8f3c95
Ketan Rakesh <rakeshketan28@gmail.com>
2026-08-04 16:41:35 +0530

    feat: add password hashing for users

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -20,7 +21,12 @@ const userSchema = new mongoose.Schema({
     },{
         timestamps: true
     });
-
+userSchema.pre('save', async function () {
+    if (!this.isModified("password")){
+        return;
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+});
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
