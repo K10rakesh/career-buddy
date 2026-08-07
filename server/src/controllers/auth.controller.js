23d07a89b178d90b5379d0d08c369aa4f21e6e5d
Ketan Rakesh <rakeshketan28@gmail.com>
2026-08-07 23:46:00 +0530

    feat: implement user login with JWT authentication (#80)

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,70 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res) => {
+    try{
+        const {email} = req.validatedData;
+        const existingUser = await User.findOne({email});
+        if (existingUser){
+            return res.status(409).json({
+                "message": "Email already registered."
+            });
+        }
+        const user = new User(req.validatedData);        
+        await user.save();
+        res.status(201).json({
+            "message": "User registered successfully.",
+            "user": {
+                "id": user.id,
+                "name": user.name,
+                "email": user.email
+            }
+        });
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({
+            message: "Failed to register user."
+        });
+    }
+}
+
+const login = async (req, res) => {
+    try{
+        const {email} = req.validatedData;
+        const existingUser = await User.findOne({email});
+        if (!existingUser){
+            return res.status(401).json({
+                "message": "Invalid email or password."
+            });
+        }
+        const isPasswordValid = await bcrypt.compare(req.validatedData.password, existingUser.password);
+        if (!isPasswordValid){
+            return res.status(401).json({
+                "message": "Invalid email or password."
+            });
+        }
+        const payload = {
+            userId: existingUser.id
+        };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "1h"});
+        res.status(200).json({
+            "message": "Login successful.",
+            "token": token,
+            "user": {
+                "id": existingUser.id,
+                "name": existingUser.name, 
+                "email": existingUser.email
+            }
+        });
+    }
+    catch (err){
+        res.status(500).json({
+            "message": "Login failed."
+        })
+    }
+}
+
+module.exports = {register, login};
+
--- server/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const register = require("../controllers/authController");
-const createAuthValidator = require('../validators/auth.validator');
-const validationMiddleware = require('../middleware/validation.middleware');
-
-router.post("/register", createAuthValidator, validationMiddleware, register);
-
-module.exports = router; 
