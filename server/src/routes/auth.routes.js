3d0e6324e08c5837d407513aedec40fe1708e7c9
Ketan Rakesh <rakeshketan28@gmail.com>
2026-08-08 20:25:40 +0530

    add JWT authentication middleware (#82)

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {register, login} = require("../controllers/auth.controller");
 const {registerValidator, loginValidator} = require('../validators/auth.validator');
 const validationMiddleware = require('../middleware/validation.middleware');
+const authMiddleware = require("../middleware/auth.middleware");
 
 router.post("/register", registerValidator, validationMiddleware, register);
-router.post("/login", loginValidator, validationMiddleware, login)
+
+router.post("/login", loginValidator, validationMiddleware, login);
 
 module.exports = router;    
