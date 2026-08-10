41a564f60bfa9fcc3e85b0f840d90c3a18fe2089
Ketan Rakesh <rakeshketan28@gmail.com>
2026-08-11 00:02:40 +0530

    feat: add task ownership and CRUD operations

--- server/src/routes/task.routes.js
+++ server/src/routes/task.routes.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const createTaskController = require("../controllers/task.controller");
-const createTaskValidator = require('../validators/task.validator');
+const {
+    createTaskController, 
+    getTasksController, 
+    getTaskByIdController, 
+    updateTaskController, 
+    deleteTaskController
+} = require("../controllers/task.controller");
+const {createTaskValidator, updateTaskValidator} = require('../validators/task.validator');
 const validationMiddleware = require('../middleware/validation.middleware');
+const authMiddleware = require("../middleware/auth.middleware");
 
-router.post("/", createTaskValidator, validationMiddleware, createTaskController);
+router.post("/", authMiddleware, createTaskValidator, validationMiddleware, createTaskController);
+
+router.get("/", authMiddleware, getTasksController);
+
+router.get("/:id", authMiddleware, getTaskByIdController);
+
+router.patch("/:id", authMiddleware, updateTaskValidator, validationMiddleware, updateTaskController);
+
+router.delete("/:id", authMiddleware, deleteTaskController);
 
 module.exports = router; 
--- server/src/validators/task.validator.js
+++ server/src/validators/task.validator.js
@@ -1,8 +1,23 @@
 const {body} = require('express-validator');
 const allowedFields = require('./helpers/allowedFields');
+
 const createTaskValidator = [
     body('title').exists().isString().trim().notEmpty().isLength({max: 50}),
     body('description').optional().isString().trim().isLength({max: 200}),
     allowedFields(['title', 'description'])
 ];
-module.exports = createTaskValidator;
+
+const updateTaskValidator = [
+    body('title').optional().isString().trim().notEmpty().isLength({max: 50}),
+    body('description').optional().isString().trim().isLength({max: 200}),
+    body('completed').optional().isBoolean({strict: true}),
+    allowedFields(["title", "description", "completed"]),
+    body().custom((value) => {
+        if (Object.keys(value).length === 0){
+            throw new Error("At least one field is required.");
+        }
+        return true;
+    })
+];
+
+module.exports = {createTaskValidator, updateTaskValidator}; 
